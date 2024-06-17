4df71db8274c4e7a90ee1784d2bf5ec56331d92c
dsykes <drewsykes28@gmail.com>
2024-06-17 14:02:01 -0700

    Added rope to south america invin & created print statement in america

--- file_testing_america.py
+++ file_testing_america.py
@@ -1,3 +1,5 @@
 import os
 
 print(os.curdir)
+
+print('This is the america')
--- south_africa.py
+++ south_africa.py
@@ -2,3 +2,4 @@ print('The country is here')
 #What type of color is there
 
 print("Ive heard the mountains here are great!")
+invintory = ['rope']
